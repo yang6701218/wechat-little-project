c60ef4d654c79634a4a9e45014a1abf1c80ec434
yang6701218 <648761695@qq.com>
2016-10-10 09:05:30 +0800

    10/10

--- pages/hot/hot.js
+++ pages/hot/hot.js
@@ -3,18 +3,22 @@ Page({
     indicatorDots:true,
     durTime:500,
     pageActive:0,
-    scrollViewHeight:400
+    scrollViewHeight:400,
+    liveList:[],
+    womanList:[],
+    liveList:[]
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
   },
   onReady:function(){
-    // 页面渲染完成
+      this.getList();
   },
   scroll:function(event){
     console.log(event.detail);
   },
   swiperChange:function(event){
+    var current =  event.detail.current;
     this.pageActiveChange(event.detail.current);
   },
   swiperItem:function(event){
@@ -24,5 +28,18 @@ Page({
      this.setData({
        pageActive:index
      })
+  },
+  getList:function(index){
+     var index =  index | 0;
+     wx.request({
+         url:'http://10.7.186.101:8080/api/getList',
+         data:{},
+         complete:function(){
+             
+         },
+         success:function(){
+           
+         }
+     });
   }
 })
